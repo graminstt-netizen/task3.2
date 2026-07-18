0f9f5e8c7436ca2e6b7fc46eff0f2b016a8bf22b
Бабурин Дмитрий <graminstt@gmail.com>
2026-07-18 23:27:17 +0500

    Создана структура проекта, написан README

---
+++ app/src/main.c
@@ -0,0 +1,14 @@
+/*
+main.c - главный модуль программы. 
+
+Бабурин Дмитрий Сергеевич
+МК-101
+*/
+
+#include "lib_main.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("BigNum Calculator started\n");
+    return 0;
+}
---
+++ lib/include/lib_main.h
@@ -0,0 +1,26 @@
+/*
+main.c - главный модуль программы. 
+
+Бабурин Дмитрий Сергеевич
+МК-101
+*/
+
+
+#ifndef LIB_MAIN_H
+#define LIB_MAIN_H
+
+#include <stdlib.h>
+
+// Определение типов
+typedef unsigned int BitsArrayElementType;
+typedef BitsArrayElementType *BitsArray;
+typedef unsigned long long BitsArrayMaxType;
+typedef BitsArray BigNum;
+
+#define N   32
+
+#define IN
+#define OUT
+
+
+#endif // LIB_MAIN_H
